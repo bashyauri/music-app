7e95484beab80a8a68a970195836d30a612d5ccd
Bashar Umar <basharu83@gmail.com>
2024-09-22 20:57:29 +0100

    My first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+import { useModalStore } from '@/stores/modal';
+
+const store = useModalStore();
+
+
+
+const toggleAuthModal = () => {
+    store.isOpen = !store.isOpen
+    console.log(store.isOpen);
+
+};
+
+
+
+</script>
+<template>
+    <!-- Header -->
+    <header id="header" class="bg-gray-700">
+        <nav class="container flex items-center justify-start px-4 py-5 mx-auto">
+            <!-- App Name -->
+            <a class="mr-4 text-2xl font-bold text-white uppercase" href="#">Music</a>
+
+            <div class="flex items-center flex-grow">
+                <!-- Primary Navigation -->
+                <ul class="flex flex-row mt-1">
+                    <!-- Navigation Links -->
+                    <li>
+                        <a class="px-2 text-white" href="#" @click="toggleAuthModal">Login / Register</a>
+                    </li>
+                    <li>
+                        <a class="px-2 text-white" href="#">Manage</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</template>
---
+++ src/stores/modal.js
@@ -0,0 +1,13 @@
+// stores/counter.js
+import { defineStore } from 'pinia'
+
+export const useModalStore = defineStore('modal', {
+  state: () => ({
+    isOpen: false
+  }),
+  getters: {
+    hiddenClass: (state) => {
+      return !state.isOpen ? 'hidden' : ''
+    }
+  }
+})
