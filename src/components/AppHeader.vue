1547cfa5824ea9b076ace5de439afbf5a38ce67a
Bashar Umar <basharu83@gmail.com>
2024-10-04 19:00:30 +0100

    adding routes for views

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,18 +23,21 @@ const toggleAuthModal = () => {
     <header id="header" class="bg-gray-700">
         <nav class="container flex items-center justify-start px-4 py-5 mx-auto">
             <!-- App Name -->
-            <a class="mr-4 text-2xl font-bold text-white uppercase" href="#">Music</a>
+            <RouterLink class="mr-4 text-2xl font-bold text-white uppercase" to="home">Music</RouterLink>
 
             <div class="flex items-center flex-grow">
                 <!-- Primary Navigation -->
                 <ul class="flex flex-row mt-1">
                     <!-- Navigation Links -->
+                    <li>
+                        <RouterLink class="px-2 text-white" to="about">About</RouterLink>
+                    </li>
                     <li v-if="!userStore.userLoggedIn">
                         <a class="px-2 text-white" href="#" @click="toggleAuthModal">Login / Register</a>
                     </li>
                     <template v-else>
                         <li>
-                            <a class="px-2 text-white" href="#">Manage</a>
+                            <RouterLink class="px-2 text-white" to="manage">Manage</RouterLink>
                         </li>
                         <li>
                             <a class="px-2 text-white hover:cursor-pointer"
