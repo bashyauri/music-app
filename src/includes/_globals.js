336974c4c11c348865e978191ea0f2c4413830d3
Bashar Umar <basharu83@gmail.com>
2024-11-05 12:51:36 +0100

    install visualizer plugin

--- src/includes/_globals.js
+++ src/includes/_globals.js
@@ -1,12 +1,14 @@
-import _ from 'lodash'
+import { upperFirst } from 'lodash/upperFirst'
+import { camelCase } from 'lodash/camelCase'
+
 export const GlobalComponent = {
   install(app) {
     const baseComponents = import.meta.glob('../components/base/**/*.vue', {
       eager: true
     })
     Object.entries(baseComponents).forEach(([path, module]) => {
-      const componentName = _.upperFirst(
-        _.camelCase(
+      const componentName = upperFirst(
+        camelCase(
           path
             .split('/')
             .pop()
--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,22 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+// import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // visualizer({
+    //   filename: 'bundle-visualizer.html',
+    //   open: true
+    // }),
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
       },
+
       manifest: {
         name: 'Music App',
         short_name: 'Music App',
@@ -31,6 +37,7 @@ export default defineConfig({
         globPatterns: ['**/*.{js,css,html,png,jpg}']
       }
     }),
+
     vueDevTools()
   ],
   resolve: {
