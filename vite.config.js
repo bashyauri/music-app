6ae67d6cc1e17cf1d3d3a8018967fd727a2d8d60
Bashar Umar <basharu83@gmail.com>
2024-11-08 11:38:47 +0100

    run vitest

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true
+  },
   plugins: [
     visualizer({
       filename: 'bundle-visualizer.html',
